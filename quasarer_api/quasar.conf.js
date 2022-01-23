0c28c5fa7cdb2400b15ebbf18e461c660d06161f
NAK VANNA <nakvannamcu@gmail.com>
2022-01-23 11:38:12 +0700

    update

--- quasarer_api/quasar.conf.js
+++ quasarer_api/quasar.conf.js
@@ -21,7 +21,7 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'axios',
-      'route_middleware'
+      `route_middleware`
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
--- quasarer_api/src/boot/route_middleware.ts
+++ quasarer_api/src/boot/route_middleware.ts
@@ -5,6 +5,8 @@ import {LOGIN_INFO_KEY} from "src/helpers/utils";
 export default boot(({router}) => {
   let userInfo = LocalStorage.getItem(LOGIN_INFO_KEY)
   router.beforeEach(async (to, from, next) => {
+
+    console.log(to)
     if (to.meta['requireAuth']) {
       if (!userInfo){
         next('/login')
